fd8a3d6054e74f2240b34c6131bdcd05b6354a77
1DimBeam1 <pro100dima_2016@mail.ru>
2022-11-02 09:25:56 +1000

    final

--- polindrom/main.cpp
+++ polindrom/main.cpp
@@ -5,29 +5,26 @@ using namespace std;
 
 int main() {
    char str[A][B];
-   int num = 0, space1 = 0, space2,szf;
-   bool bolk = true;
+   int num = 0,szf, lenstr;
+   bool flag;
    for(int i = 0; i<=A-1; i++){
-       cin >> str[i];
+       cin.getline(str[i], B);
    }
+
     for(int i = 0; i<=A-1; i++) {
-        space1 = 0; space2 = 0;
-        for (int j = 0; j < B && str[i][j]!='\0'; j++) {
-            if (str[i][j] == ' ' || str[i][j] == '\0') {
-                space2 = j;
-                szf = space2 - space1;
-                for (int k = space1; k <= (space2-(szf/2)-1) && bolk; k++) {// нормальное условие в цикле
-                    if (str[i][k] != str[i][space2-(k-space1)-1]) {
-                        bolk = false;
-                    }
-                }
-                space1 = space2+1;
-                if (bolk) { num++;} else{bolk = true;}
-            }
+        lenstr = 0;
+        flag = true;
+        while (str[i][lenstr] != '\0'){lenstr++;}
+        if(lenstr % 2 == 1 ){szf = (lenstr+1)/2;}
+        else {szf = (lenstr)/2;}
+
+        for(int j = 0; j<= szf && flag; j++){
+            if(str[i][j] != str[i][lenstr - j-1]){flag = false;}
         }
+        if (flag){num++;}
     }
 
-   cout << num;
+   cout << "Palindrom:"<< num ;
 
 
 }
